f0d00e00ec489f1031139085ba5119d259a74d3e
roger melo <meloroger059@gmail.com>
2019-10-13 21:40:51 +0000

    redeploy

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { static as Estatic } from 'express';
 import { join } from 'path';
+import { log } from 'console';
 
 async function bootstrap() {
+  log('!!!bootstrap!!!');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(Estatic(join(__dirname, 'client-build')));
   await app.listen(process.env.PORT || 3000);
+  log('Server Started....');
 }
 bootstrap();
